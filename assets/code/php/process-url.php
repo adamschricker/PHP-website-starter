6184511035109170f41b5553ab81dbc0b54211af
Adam Schricker <adamschricker@yahoo.com>
2021-02-27 13:11:14 -0500

    add 404 response code

--- assets/code/php/process-url.php
+++ assets/code/php/process-url.php
@@ -11,6 +11,7 @@ if (substr($urlPath, -1) !== "/") {
 
 /** 404 if URL has characters not alphanumeric, dash, or forward slash **/
 if (preg_match('/[^a-z0-9\/\-]+/i', $urlPath)) {
+	http_response_code(404);
 	$urlPath = "/404/";
 }
 
@@ -25,6 +26,7 @@ if ($urlPath === "/") {
 $pageHeadFile = $pageDirectory . "head.php";
 $pageContentFile = $pageDirectory . "content.php";
 if(!file_exists($pageHeadFile) || !file_exists($pageContentFile)) {
+	http_response_code(404);
 	$pageDirectory = "pages/404/";
 	$pageHeadFile = $pageDirectory . "head.php";
 	$pageContentFile = $pageDirectory . "content.php";
